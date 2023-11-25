067a35ad8887444df22dccb6481f595edcc40326
Srivatsa <srivatsarupadhya@gmail.com>
2023-11-25 12:36:58 +0530

    create basic register form

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,9 @@
 import { signIn, signOut, useSession } from "next-auth/react";
 import Head from "next/head";
 import { Button } from "@/components/ui/button" 
-import { api } from "~/utils/api";
+import Register from "~/components/Register";
 
 export default function Home() {
-  const hello = api.post.hello.useQuery({ text: "from tRPC" });
-
   return (
     <>
       <Head>
@@ -14,7 +12,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.png" />
       </Head>
       <main className=" flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-       <Button className="bg-white">Register</Button>
+		<Register />
       </main>
     </>
   );
