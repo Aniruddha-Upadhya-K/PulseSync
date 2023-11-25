a17cc1eb723e5a92113dd756a0cca08a5ff3ecbd
Srivatsa <srivatsarupadhya@gmail.com>
2023-11-25 16:08:59 +0530

    finish registration

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -1,70 +1,24 @@
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-/* eslint-disable @typescript-eslint/ban-ts-comment */
 // @ts-nocheck
-
-import React, { useEffect, useMemo, useRef, useState } from "react";
-import { useAnimations, useFBX, useGLTF } from "@react-three/drei";
-import { useFrame, useLoader } from "@react-three/fiber";
-import * as THREE from "three";
+import React, { useRef } from 'react'
+import { useGLTF } from '@react-three/drei'
 
 export default function Model() {
-  const { nodes, materials } = useGLTF("/assets/models/avatar.glb");
+  const { nodes, materials } = useGLTF('/assets/models/avatar.glb')
   return (
     <group dispose={null}>
       <primitive object={nodes.Hips} />
-      <skinnedMesh
-        geometry={nodes.Wolf3D_Body.geometry}
-        material={materials.Wolf3D_Body}
-        skeleton={nodes.Wolf3D_Body.skeleton}
-      />
-      <skinnedMesh
-        geometry={nodes.Wolf3D_Outfit_Bottom.geometry}
-        material={materials.Wolf3D_Outfit_Bottom}
-        skeleton={nodes.Wolf3D_Outfit_Bottom.skeleton}
-      />
-      <skinnedMesh
-        geometry={nodes.Wolf3D_Outfit_Footwear.geometry}
-        material={materials.Wolf3D_Outfit_Footwear}
-        skeleton={nodes.Wolf3D_Outfit_Footwear.skeleton}
-      />
-      <skinnedMesh
-        geometry={nodes.Wolf3D_Outfit_Top.geometry}
-        material={materials.Wolf3D_Outfit_Top}
-        skeleton={nodes.Wolf3D_Outfit_Top.skeleton}
-      />
-      <skinnedMesh
-        geometry={nodes.Wolf3D_Hair.geometry}
-        material={materials.Wolf3D_Hair}
-        skeleton={nodes.Wolf3D_Hair.skeleton}
-      />
-      <skinnedMesh
-        geometry={nodes.Wolf3D_Glasses.geometry}
-        material={materials.Wolf3D_Glasses}
-        skeleton={nodes.Wolf3D_Glasses.skeleton}
-      />
-      <skinnedMesh
-        geometry={nodes.EyeLeft.geometry}
-        material={materials.Wolf3D_Eye}
-        skeleton={nodes.EyeLeft.skeleton}
-      />
-      <skinnedMesh
-        geometry={nodes.EyeRight.geometry}
-        material={materials.Wolf3D_Eye}
-        skeleton={nodes.EyeRight.skeleton}
-      />
-      <skinnedMesh
-        geometry={nodes.Wolf3D_Head.geometry}
-        material={materials.Wolf3D_Skin}
-        skeleton={nodes.Wolf3D_Head.skeleton}
-      />
-      <skinnedMesh
-        geometry={nodes.Wolf3D_Teeth.geometry}
-        material={materials.Wolf3D_Teeth}
-        skeleton={nodes.Wolf3D_Teeth.skeleton}
-      />
+      <skinnedMesh geometry={nodes.Wolf3D_Body.geometry} material={materials.Wolf3D_Body} skeleton={nodes.Wolf3D_Body.skeleton} />
+      <skinnedMesh geometry={nodes.Wolf3D_Outfit_Bottom.geometry} material={materials.Wolf3D_Outfit_Bottom} skeleton={nodes.Wolf3D_Outfit_Bottom.skeleton} />
+      <skinnedMesh geometry={nodes.Wolf3D_Outfit_Footwear.geometry} material={materials.Wolf3D_Outfit_Footwear} skeleton={nodes.Wolf3D_Outfit_Footwear.skeleton} />
+      <skinnedMesh geometry={nodes.Wolf3D_Outfit_Top.geometry} material={materials.Wolf3D_Outfit_Top} skeleton={nodes.Wolf3D_Outfit_Top.skeleton} />
+      <skinnedMesh geometry={nodes.Wolf3D_Hair.geometry} material={materials.Wolf3D_Hair} skeleton={nodes.Wolf3D_Hair.skeleton} />
+      <skinnedMesh geometry={nodes.Wolf3D_Glasses.geometry} material={materials.Wolf3D_Glasses} skeleton={nodes.Wolf3D_Glasses.skeleton} />
+      <skinnedMesh geometry={nodes.EyeLeft.geometry} material={materials.Wolf3D_Eye} skeleton={nodes.EyeLeft.skeleton} />
+      <skinnedMesh geometry={nodes.EyeRight.geometry} material={materials.Wolf3D_Eye} skeleton={nodes.EyeRight.skeleton} />
+      <skinnedMesh geometry={nodes.Wolf3D_Head.geometry} material={materials.Wolf3D_Skin} skeleton={nodes.Wolf3D_Head.skeleton} />
+      <skinnedMesh geometry={nodes.Wolf3D_Teeth.geometry} material={materials.Wolf3D_Teeth} skeleton={nodes.Wolf3D_Teeth.skeleton} />
     </group>
-  );
+  )
 }
 
-useGLTF.preload("/assets/models/avatar.glb");
+useGLTF.preload('/assets/models/avatar.glb')
--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -19,9 +19,22 @@ import { Textarea } from "@/components/ui/textarea";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { useSession } from "next-auth/react";
+import { api } from "~/utils/api";
+import { z } from "zod";
+import React, { useState } from "react";
+import { Gender } from "@prisma/client";
 
 export default function Register() {
 	const user = useSession();
+	const register = api.user.updateUser.useMutation();
+	const [Name, setName] = useState("");
+	const [Age, setAge] = useState("");
+	const [Height, setHeight] = useState("");
+	const [Weight, setWeight] = useState("");
+	const [Gender, setGender] = useState<Gender|null>(null);
+	const [HealthConditions, setHealthConditions] = useState("");
+	const [Medications, setMedications] = useState("");
+
 	return (
 		<Dialog>
 			<DialogTrigger asChild>
@@ -47,31 +60,54 @@ export default function Register() {
 						>
 							Name
 						</Label>
-						<Input id="name" className="col-span-3" />
+						<Input
+							id="name"
+							className="col-span-3"
+							onChange={(e) => setName(e.target.value)}
+						/>
 					</div>
 					<div className="grid grid-cols-4 items-center gap-4">
 						<Label htmlFor="age" className="text-right">
 							Age
 						</Label>
-						<Input id="age" type="number" className="col-span-3" />
+						<Input
+							id="age"
+							type="number"
+							className="col-span-3"
+							onChange={(e) => setAge(e.target.value)}
+						/>
 					</div>
 					<div className="grid grid-cols-4 items-center gap-4">
 						<Label htmlFor="height" className="text-right">
 							Height
 						</Label>
-						<Input id="height" type="number" className="col-span-3" />
+						<Input
+							id="height"
+							type="number"
+							className="col-span-3"
+							onChange={(e) => setHeight(e.target.value)}
+						/>
 					</div>
 					<div className="grid grid-cols-4 items-center gap-4">
 						<Label htmlFor="weight" className="text-right">
-							Weight	
+							Weight
 						</Label>
-						<Input id="height" type="number" className="col-span-3" />
+						<Input
+							id="height"
+							type="number"
+							className="col-span-3"
+							onChange={(e) => setWeight(e.target.value)}
+						/>
 					</div>
 					<div className="grid grid-cols-4 items-center gap-4">
 						<Label htmlFor="gender" className="text-right">
 							Gender
 						</Label>
-						<Select>
+						<Select
+							onValueChange={(e) => {
+								setGender(e as Gender);
+							}}
+						>
 							<SelectTrigger className="w-[180px]">
 								<SelectValue placeholder="Select Gender" />
 							</SelectTrigger>
@@ -86,17 +122,50 @@ export default function Register() {
 						<Label htmlFor="age" className="text-right">
 							Health Conditions
 						</Label>
-						<Textarea placeholder="Enter your health conditions here..." />
+						<Textarea
+							placeholder="Enter your health conditions here..."
+							onChange={(e) => {
+								setHealthConditions(e.target.value);
+							}}
+						/>
 					</div>
 					<div className="grid grid-cols-4 items-center gap-4">
 						<Label htmlFor="age" className="text-right">
 							Medications
 						</Label>
-						<Textarea placeholder="Enter any medications you are undergoing..." />
+						<Textarea
+							placeholder="Enter any medications you are undergoing..."
+							onChange={(e) => {
+								setMedications(e.target.value);
+							}}
+						/>
 					</div>
 				</div>
 				<DialogFooter>
-					<Button type="submit">Save changes</Button>
+					<Button
+						type="submit"
+						onSubmit={() => {
+							register.mutate({
+								name: z.string().nonempty().parse(Name),
+								age: z.number().positive().parse(Age),
+								height: z.number().positive().parse(Height),
+								weight: z.number().positive().parse(Weight),
+								healthConditions: z
+									.string()
+									.nonempty()
+									.parse(HealthConditions),
+								medications: z
+									.string()
+									.nonempty()
+									.parse(Medications),
+								gender: z
+									.enum("Male", "Female", "Other")
+									.parse(Gender),
+							});
+						}}
+					>
+						Save changes
+					</Button>
 				</DialogFooter>
 			</DialogContent>
 		</Dialog>
