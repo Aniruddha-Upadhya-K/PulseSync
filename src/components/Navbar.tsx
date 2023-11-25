0af0043be3b55334e9bd80957a2da272674b1a91
satwikrprabhu <satwikprabhu@gmail.com>
2023-11-25 12:10:39 +0530

    feat: navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link"
+import Image from "next/image"
+
+const Navbar = () => {
+
+  return (
+    <nav className="fixed bg-white top-0 w-full z-20 text-center md:text-left border-b border-gray-400/20 dark:bg-gray-600 bg-opacity-5 backdrop-blur-lg backdrop-filter p-4 flex justify-between items-center drop-shadow-xl">
+        <h1 className="md:px-12 font-bold text-4xl text-white flex flex-row justify-between gap-4">
+          <Image src="/favicon.png" alt="" height={30} width={40}/>
+          <Link href='/'>PulseSync</Link>
+          </h1>
+    </nav>
+  )
+}
+
+export default Navbar
