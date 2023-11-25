dd0428d6db0de648639cd0d70aa85ad2b7f90890
satwikrprabhu <satwikprabhu@gmail.com>
2023-11-25 12:32:02 +0530

    feat: auth

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import { api } from "~/utils/api";
 
 export default function Home() {
   const hello = api.post.hello.useQuery({ text: "from tRPC" });
+  const { data: sessionData } = useSession();
 
   return (
     <>
@@ -14,7 +15,16 @@ export default function Home() {
         <link rel="icon" href="/favicon.png" />
       </Head>
       <main className=" flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-       <Button className="bg-white">Register</Button>
+        {
+          sessionData?<>
+          Welcome {sessionData.user.name}!
+          </>:""
+        }
+        {
+          sessionData ? 
+          <Button onClick={() => signOut()}>Logout</Button> :
+          <Button onClick={() => signIn("google")}>Talk with SyncBot</Button>
+        }
       </main>
     </>
   );
