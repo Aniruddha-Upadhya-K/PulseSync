ea2ad936518d05230c7f5bea06f6782713de8fe0
satwikrprabhu <satwikprabhu@gmail.com>
2023-11-25 18:35:19 +0530

    feat: deepgram api

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,4 +1,4 @@
-import { CornerRightUp, LogIn, Mic } from "lucide-react"
+import { CornerRightUp, LogIn, Mic, MicOff } from "lucide-react"
 import { Input } from "@/components/ui/input"
 import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
@@ -12,7 +12,9 @@ import {
   CardTitle,
 } from "@/components/ui/card"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
-import { useState } from "react"
+import { useState, useEffect } from "react"
+var websocket;
+var globalMic = true;
 
 
 type CardProps = React.ComponentProps<typeof Card>
@@ -29,6 +31,125 @@ export default function Chat({ className, ...props }: CardProps) {
         e.preventDefault();
         console.log(input);
     }
+
+    // Deepgram STT
+    const voiceMessages = {
+		micActiveMessage: "say something",
+		typeAheadMessage: "Enter your prompt here!",
+	};
+
+	
+
+	const turnOffMic = () => {
+		websocket = false;
+		console.log(`turned off : ${websocket}`);
+	};
+    const turnOnMic = () => {
+		websocket = true;
+		console.log(`turned on : ${websocket}`);
+	};
+	turnOnMic();
+
+	const [transcript, setTranscript] = useState("");
+	const [lang, setLang] = useState("en");
+	const [queryResults, setQueryResults] = useState([]);
+	const [placeholder, setPlaceholder] = useState("loading");
+	const [micActive, setMicActive] = useState(true);
+
+	useEffect(() => {
+		const MIMEtype = "audio/webm";
+
+		const webSocketURL =
+			lang === "en"
+				? "wss://api.deepgram.com/v1/listen?model=nova"
+				: `wss://api.deepgram.com/v1/listen?model=base&language=${lang}`;
+
+const webSocket = new WebSocket(webSocketURL, [
+    "token",
+    process.env.NEXT_PUBLIC_SPEECH_KEY ?? "",
+]);
+
+		navigator.mediaDevices
+			.getUserMedia({ audio: true })
+			.then((stream) => {
+				if (!MediaRecorder.isTypeSupported(MIMEtype)) {
+					// error audio not supported
+				}
+
+				const mediaRecorder = new MediaRecorder(stream, {
+					mimeType: MIMEtype,
+				});
+
+				webSocket.addEventListener("open", () => {
+					console.log("[socket]: connetction success");
+					setPlaceholder("say something");
+					mediaRecorder.addEventListener("dataavailable", (event) => {
+						if (webSocket.readyState === 1 && event.data.size) {
+							webSocket.send(event.data);
+						}
+					});
+					mediaRecorder.start(1000);
+				});
+
+				webSocket.addEventListener("message", (message) => {
+					console.log("[socket]: message received", globalMic);
+					const received = message && JSON.parse(message?.data);
+					const result = received.channel?.alternatives[0].transcript;
+					setTranscript((prevState) => {
+						if (globalMic)
+							return result === "" ? prevState : result;
+					});
+				});
+
+				webSocket.addEventListener("error", (err) => {
+					console.log("[socket]: ", err);
+					setPlaceholder("sorry there has been an error");
+					setTimeout(() => {
+						setPlaceholder("Enter your prompt here!");
+					}, 5000);
+				});
+
+				webSocket.addEventListener("close", () => {
+					setPlaceholder(
+						globalMic
+							? "try reloading the page"
+							: "Enter your prompt here!"
+					);
+					console.log("[socket]: close");
+				});
+			})
+			.catch((err) => {
+				// handle error
+				// permission ignored accessed
+			});
+	}, [lang]);
+
+	// useEffect(() => {
+	// 	if (transcript !== "") {
+	// 		//callLink(transcript);
+	// 		debounce(callList(transcript));
+	// 	}
+	// }, [transcript]);
+
+	useEffect(() => {
+		// set mic status
+		globalMic = micActive;
+		if (!micActive) {
+			// webSocket.close();
+			turnOffMic();
+			setPlaceholder("Enter your prompt here!");
+		} else {
+			turnOnMic();
+			setPlaceholder("say something");
+		}
+	}, [micActive]);
+
+    setTimeout(() => {
+        if (transcript === "") {
+            turnOffMic();
+        }
+    }, 3000);
+    
 
   return (
     <div className="flex justify-center min-h-screen items-center">
@@ -61,10 +182,21 @@ export default function Chat({ className, ...props }: CardProps) {
       <CardFooter className="space-x-2">
       <div className="flex w-full max-w-sm items-center space-x-2">
         <form className="w-full space-x-2 flex" onSubmit={handleSubmit}>
-      <Input type="text" placeholder="Enter a prompt here!" value={input} onChange={handleInputChange} />
+      <Input type="text" placeholder={placeholder}  id="search-bar" value={transcript}
+					onChange={(e) => setTranscript(e.target.value)}
+					onKeyDown={(e) => setMicActive(false)}/>
       <div className="flex flex-row gap-1">
       <Button type="submit" size="icon"><CornerRightUp className="h-4 w-4" /></Button>
-      <Button type="submit" size="icon"><Mic  className="h-4 w-4" /></Button></div>
+      {
+        micActive ?
+        <Button type="submit" size="icon" onClick={(e)=>{
+            e.preventDefault();
+            setMicActive(!micActive);
+      }}><Mic  className="h-4 w-4" /></Button> : <Button type="submit" size="icon" onClick={(e)=>{
+        e.preventDefault();
+        setMicActive(!micActive);
+  }}><MicOff  className="h-4 w-4" /></Button>
+    }</div>
       </form>
     </div>
       </CardFooter>
