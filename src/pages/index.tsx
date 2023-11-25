254ba7b7f8a8802ed75810520aef300837eb6129
Aniruddha Upadhya K <aniupadhyak1234@gmail.com>
2023-11-25 17:34:41 +0530

    fix: add avatar and animations

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,20 +3,34 @@ import { Button } from "@/components/ui/button";
 import Register from "~/components/Register";
 import { api } from "~/utils/api";
 import Chat from "~/components/Chat";
-import { Canvas } from "@react-three/fiber";
-import { Environment, OrbitControls } from "@react-three/drei";
+import { Canvas, useThree } from "@react-three/fiber";
+import { Environment, OrbitControls, useTexture } from "@react-three/drei";
 /* eslint-disable-next-line @typescript-eslint/ban-ts-comment */
 /* @ts-ignore */
 
-import { Avatar1 } from "~/components/3d/Avatar1";
+import { Avatar } from "~/components/3d/Avatar";
 
 export default function Home() {
   const hello = api.post.hello.useQuery({ text: "from tRPC" });
   const { data: sessionData } = useSession();
 
+  // const texture = useTexture("/assets/texture.png")
+  // const viewport = useThree((state) => state.viewport)
+
   return (
     <>
-      <main className=" flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
+      <div className="absolute inset-0 -z-50">
+        <Canvas shadows camera={{ position: [0, 0, 5], fov: 43 }}>
+          <color attach="background" args={["#ececec"]} />
+          <OrbitControls />
+          {/* eslint-disable-next-line @typescript-eslint/ban-ts-comment */}
+          {/* @ts-ignore */}
+          <Avatar position={[-1, -3, 1]} scale={2} />
+          <Environment preset="apartment" />
+          <Scene />
+        </Canvas>
+      </div>
+      <main className="relative z-0 flex min-h-screen flex-col items-center justify-center">
         {/* {
           sessionData?<>
           <p className="text-white text-4xl font">Welcome {sessionData.user.name}!</p>
@@ -28,16 +42,19 @@ export default function Home() {
           <Button onClick={() => signIn("google")}>Talk with SyncBot</Button>
         )}
         {sessionData?.user.image ? <Chat /> : ""}
-        <Canvas shadows camera={{ position: [0, 0, 8], fov: 43 }}>
-          <color attach="background" args={["#ececec"]} />
-          <OrbitControls />
-          {/* eslint-disable-next-line @typescript-eslint/ban-ts-comment */}
-          {/* @ts-ignore */}
-          <Avatar1 />
-          <Environment preset="apartment" />
-        </Canvas>
         <Register />
       </main>
     </>
   );
 }
+
+const Scene = () => {
+  const viewport = useThree((state) => state.viewport);
+  const texture = useTexture("/assets/texture.png");
+  return (
+    <mesh>
+      <planeGeometry args={[viewport.width, viewport.height]} />
+      <meshBasicMaterial map={texture} />
+    </mesh>
+  );
+};
